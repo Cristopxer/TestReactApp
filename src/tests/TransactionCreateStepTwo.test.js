87e08588ec5ff234affe9b57321a813212e3a0f5
cristopxer <cristopherortiz457@gmail.com>
2023-01-26 18:52:01 -0600

    e2e payment test

--- src/tests/TransactionCreateStepTwo.test.js
+++ src/tests/TransactionCreateStepTwo.test.js
@@ -2,14 +2,29 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "components/TransactionCreateStepTwo";
 
-test("on initial render, the pay button is disabled", async () => {
-    render(<TransactionCreateStepTwo sender={{ id: "3334123" }} receiver={{ id: "222224" }} />);
 
-    expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
-});
+// Unit tests
+// test("on initial render, the pay button is disabled", async () => {
+//     render(<TransactionCreateStepTwo sender={{ id: "3334123" }} receiver={{ id: "222224" }} />);
+
+//     expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+// });
+
+// test("if an amount and note is entered the pay button change to enabled", async () => {
+//     render(<TransactionCreateStepTwo sender={{ id: "3334123" }} receiver={{ id: "222224" }} />);
+
+//     userEvent.type(screen.getByPlaceholderText(/amount/i), "50")
+//     userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner")
+
+//     expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+// });
+
+// Integration test
 
 test("if an amount and note is entered the pay button change to enabled", async () => {
     render(<TransactionCreateStepTwo sender={{ id: "3334123" }} receiver={{ id: "222224" }} />);
+
+    expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
 
     userEvent.type(screen.getByPlaceholderText(/amount/i), "50")
     userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner")
